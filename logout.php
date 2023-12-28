bd62106c6cd62cfdccbf4aae4c7121e765149392
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-12-28 22:52:57 +0800

    logout

--- logout.php
+++ logout.php
@@ -5,8 +5,7 @@ $db = new global_class();
 session_start();
 $userId = $_SESSION['id'];
 $loginId = $_SESSION['login_id'];
-if ($db->logout($userId, $loginId)) {
-}
+$db->logout($userId, $loginId);
 
 session_destroy();
 header('Location: index.php');
